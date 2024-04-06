7ce9361241186ed343f52ecfd0d983d77eb534a1
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-04-06 07:50:52 +0700

    home

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./pages/Layout";
+import PrivateRoute from "./middleware";
+import Home from "./pages/user/Home";
+
+const ROLES = {
+  User: 2001,
+  Editor: 1984,
+  Admin: 5150,
+};
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         {/* public routes */}
-        {/* <Route path="login" element={<Login />} /> */}
+        <Route path="login" element={<Login />} />
         {/* <Route path="register" element={<Register />} />
       <Route path="linkpage" element={<LinkPage />} />
       <Route path="unauthorized" element={<Unauthorized />} /> */}
+
+        <Route element={<PrivateRoute allowedRoles={[ROLES.User]} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
 
         {/* catch all */}
         {/* <Route path="*" element={<Missing />} /> */}
